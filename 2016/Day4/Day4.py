c1b1ce96246155f837cd28d827861f64a1e22fe8
Gaurav Kshirsagar <gaurav.kshirsagar@synacor.com>
2016-12-27 17:36:06 -0500

    Added a imtermidiate day 4 part 1 code just for back reference.

--- 2016/Day4/Day4.py
+++ 2016/Day4/Day4.py
@@ -1,24 +1,50 @@
 import re
-import numpy as np
+from collections import Counter
 
 def read_file():
-    with open("./input") as f:
-        input = f.readlines()
+	with open("./input") as f:
+		input = f.readlines()
 
-    print input
-    return input
+	#print input
+	return input
 
 def find_sum_of_room_checksums(input):
+	room_list = []
+	bigsum = 0
+	for line in input[:10]:
+		# room = re.findall(r'^([a-z]+)-([a-z]+.{1})+(\d+)(\[[a-z]+\])\n$', line)
+		split_string = re.findall(r'([a-z-]+)+(\d+)(\[[a-z]+\])\n', line)
+		letters = ''.join(split_string[0][0].split('-'))
+		numbers = split_string[0][1]
+		checksum = split_string[0][2][1:-1]
+		room_list.append([letters, numbers, checksum])
 
-    room_list = []
-    for line in input:
-        # room = re.findall(r'^([a-z]+)-([a-z]+.{1})+(\d+)(\[[a-z]+\])\n$', line)
-        room = re.findall(r'([a-z]+[-])+(\d+)(\[[a-z]+\])\n', line)
-        room_list.append(room)
+		sorted_letters = sorted(letters)
+		#print '\nletters, sorted-letters: ', letters, sorted_letters,
+		valid = True
+		should_match_checksum = ''
+		list_sorted_letters = sorted_letters.items()i
+		ctr = 0
+		print sorted_letters.iteritems()
+		for k in sorted_letters:
+			if ctr >= len(checksum):
+				break
 
-    print room_list
+			print k, '; ',
+			should_match_checksum += str(k)
 
-    return len(room_list)
+			ctr += 1
+
+		print should_match_checksum
+		if should_match_checksum == checksum:
+			bigsum += int(numbers)
+			print split_string, 'was valid', checksum, valid, bigsum
+		else:
+			print split_string, 'was invalid', checksum, valid, bigsum
+
+	#print room_list
+
+	return len(room_list)
 
 if __name__ == '__main__':
-    print find_sum_of_room_checksums(read_file())
+	print find_sum_of_room_checksums(read_file())
