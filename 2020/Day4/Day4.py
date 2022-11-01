97bad96d977ee5a60befe839ae45540ea2ee2bfb
Gaurav Kshirsagar <gauravk1001@gmail.com>
2022-11-01 02:51:49 -0400

    Added Day 4 part 2

--- 2020/Day4/Day4.py
+++ 2020/Day4/Day4.py
@@ -29,7 +29,7 @@ def check_valid_password(text):
             if i <= len(text) - 1:
                 continue
 
-        #print('line=' + blob)
+        print('line=' + blob)
 
         # Check if all the required_fields are in the current blob
         # Count the fields that are in the current blob too
@@ -37,38 +37,89 @@ def check_valid_password(text):
         for field in required_fields.keys():
             if field in blob:
                 if not required_fields[field]:
-                    required_fields[field] = True
+                    reg = field + '([a-z0-9#]+)'
+                    values = re.search(reg, blob)
+                    print('val found for ' + field + ' = ' + str(values))
+                    bool = validateFields(field[0:-1], values.group(1))
+                    #print('val fields returns=' + str(bool))
+                    required_fields[field] = bool
                 else:
-                    #print('line contains many ' + field)
+                    print('line contains many ' + field)
                     required_fields[field] = False
                     break
             else:
-                #print('line does not contain' + field)
-                #required_fields[field] = False
+                print('line does not contain' + field)
+                required_fields[field] = False
                 break
 
+        #print('req fields=')
+        #print(required_fields)
         # current blob doesn't have all the required fields, then it is not a passport
         # even one single false in the dict, we dont have a passport
         if False in required_fields.values():
             count_non_passport += 1
-            #print('line not pass' + str(count_non_passport))
+            print('line not pass' + str(count_non_passport))
         else: # it is a passport because all the required fields are in the blob
             count_passport += 1
-            #print('line counted' + str(count_passport))
+            print('line counted' + str(count_passport))
 
         # reset blob to empty and increment to next line
         blob = ''
         i += 1
         required_fields = required_fields.fromkeys(required_fields, False)
-        #print('\n--')
+        print('\n--')
 
     print('pass=' + str(count_passport))
     print('non pass=' + str(count_non_passport))
 
+def validateFields(field, value):
+    ecls = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
+
+    #print('field in validateFields' + field + ' ' + value)
+    if field == 'byr':
+        if len(value) <= 4 and int(value) >= 1920 and int (value) <= 2002:
+            #print('valid byr' + value)
+            return True
+    elif field == 'iyr':
+        if len(value) <= 4 and int(value) >= 2010 and int (value) <= 2020:
+            #print('valid iyr' + value)
+            return True
+    elif field == 'eyr':
+        if len(value) <= 4 and int(value) >= 2002 and int (value) <= 2030:
+            #print('valid eyr' + value)
+            return True
+    elif field == 'hgt':
+        #print('hgt num=' + str(value[0:-2]) + ' ' + value[-2:] + 'haha')
+        if value[-2:] == 'cm':
+            if int(value[0:-2]) >= 150 and int(value[0:-2]) <= 193:
+                #print('valid hgt' + value)
+                return True
+        elif value[-2:] == 'in':
+            if int(value[0:-2]) >= 59 and int(value[0:-2]) <= 76:
+                #print('valid hgt' + value)
+                return True
+    elif field == 'hcl':
+        matches = re.search('(#[0-9a-f]{6})', value)
+        if matches is not None:
+            #print('matched=' + matches.group(0))
+            return True
+    elif field == 'ecl':
+        if value in ecls:
+            #print('ecl=' + value)
+            return True
+    elif field == 'pid':
+        matches = re.search('([0-9]{9})', value)
+        if matches is not None:
+            #print('matched=' + matches.group(0))
+            return True
+    elif field == 'cid':
+        return True
+    
+    return False
 
 def get_input():
     print(os.getcwd())
-    input_file = open(os.getcwd() + os.path.sep + "2020" + os.path.sep + "Day4" + os.path.sep + "input.txt", 'r')
+    input_file = open(os.getcwd() + os.path.sep + "2020" + os.path.sep + "Day4" + os.path.sep + "input copy.txt", 'r')
 
     return input_file.readlines()
 
